8629eb89f5a3bec0f927d8d722d4b5a8c0845e38
abymoen <a.bymoen@hotmail.com>
2020-07-09 15:10:52 -0600

    Added new warning message

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ echo "Hey there, let's give you a small website!"
 sudo mkdir /var/www/minion
 sudo cp index.html /var/www/minion
 echo "All done! As long as you configured the VirtualHost correctly, you should be able to see the new site when you type you domain name into a browser!"
+echo "IMPORTANT! After creating the new VirtualHost config, please remember to run the command /etc/init.d/apache2 restart"
+echo "This will force apache to read your new virtual host"
